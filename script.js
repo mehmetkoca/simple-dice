dbb298c9aa257dce38373e51c19ed3ace1713d2b
mehmetkoca <mehmetkoca@ogr.cbu.edu.tr>
2017-04-14 20:03:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function ZarAt(){
+	var f = document.getElementById("first");
+	var s = document.getElementById("second");
+	var one = Math.floor(Math.random()*6) +1;
+	var two = Math.floor(Math.random()*6)+1;
+	first.innerHTML = one;
+	second.innerHTML= two;
+}
